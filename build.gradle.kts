87e6d73e0a98785b3ae3e6cfbbf0645d17e9a162
Peter Johnson <peterandersjohnson@outlook.com>
2026-05-05 12:22:31 -0500

    UNTESTED - Elevation fixes + cave replace fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.diytechy.terra.packs"
-version = "2.0.1"
+version = "2.0.2"
 
 // Directories to exclude from the pack zip (beyond hidden dirs)
 val excludedDirs = setOf(
